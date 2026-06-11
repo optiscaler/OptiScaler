9019d6440feaf0f5e1b43b7fe55f7b7fb480aa0c
FakeMichau <49685661+FakeMichau@users.noreply.github.com>
2026-06-11 12:43:51 +0200

    Improve driver FFX model selection pattern

--- OptiScaler/fsr4/FSR4ModelSelection.cpp
+++ OptiScaler/fsr4/FSR4ModelSelection.cpp
@@ -232,8 +232,9 @@ void FSR4ModelSelection::Hook(HMODULE module, FSR4Source source)
         if (!o_createModelDriver)
         {
             // From amdxcffx64 2.1.0.968/2.2.0.1328
-            const char* pattern = "48 8B C4 48 89 58 ? 55 56 57 41 54 41 55 41 56 41 57 48 8D A8 ? ? ? ? 48 81 EC ? ? "
-                                  "? ? 0F 29 70 ? 0F 29 78 ? 48 8B 05";
+            const char* pattern =
+                "48 8B C4 48 89 58 ? 55 56 57 41 54 41 55 41 56 41 57 48 8D A8 ? ? ? ? 48 81 EC ? ? ? ? 0F 29 70 ? 0F "
+                "29 78 ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 85 ? ? ? ? 44 8B F2 48 8B F1 E8";
             o_createModelDriver = (PFN_createModel) scanner::GetAddress(module, pattern);
         }
 
