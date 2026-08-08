e305081994496a2793f0c33e9cd9e10a4d132ab1
FakeMichau <49685661+FakeMichau@users.noreply.github.com>
2026-08-08 20:28:06 +0200

    WAR for FSR4's flickering in some UE games
    Bug in FSR4's autoexposure shader

--- OptiScaler/upscalers/fsr31/FSR31Feature_Dx12.h
+++ OptiScaler/upscalers/fsr31/FSR31Feature_Dx12.h
@@ -9,6 +9,7 @@ class FSR31FeatureDx12 : public FSR31Feature, public IFeature_Dx12
 {
   private:
     NVSDK_NGX_Parameter* SetParameters(NVSDK_NGX_Parameter* InParameters);
+    ID3D12Resource* smallerColor;
 
   protected:
     bool InitFSR3(const NVSDK_NGX_Parameter* InParameters);
@@ -32,5 +33,11 @@ class FSR31FeatureDx12 : public FSR31Feature, public IFeature_Dx12
 
         if (_context != nullptr)
             FfxApiProxy::D3D12_DestroyContext(&_context, NULL);
+
+        if (smallerColor && smallerColor != nullptr)
+        {
+            smallerColor->Release();
+            smallerColor = nullptr;
+        }
     }
 };
